b119e9fcb436cc312a7b6d30a11171c67878e072
Toni Schranz <toni.schranz@gmail.com>
2022-03-15 13:54:52 +0100

    wip

--- src/AdminControllerBase.php
+++ src/AdminControllerBase.php
@@ -10,7 +10,16 @@ class AdminControllerBase extends Controller
 
     public function prepare ()
     {
-        $this->_ctx->menu[] = [];
+        $this->_ctx->menu['admin'] = [
+            ['url' => 'code', 'name' => 'code', 'children' => [
+                ['url' => 'code/controller', 'name' => 'controller'],
+                ['url' => 'code/src', 'name' => 'src'],
+                ['url' => 'code/views', 'name' => 'views']
+            ]],
+            ['url' => 'config', 'name' => 'config'],
+            ['url' => 'plugins', 'name' => 'plugins'],
+            ['url' => 'ipsum', 'name' => 'ipsum']
+        ];
     }
    
 }
