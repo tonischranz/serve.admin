ca3812f019bb046944e992b69c00f68d8c30c2ac
Toni Schranz <toni.schranz@gmail.com>
2022-03-29 11:31:04 +0200

    controller edit

--- controller/code.php
+++ controller/code.php
@@ -4,6 +4,7 @@ namespace tsd\serve\admin;
 use tsd\serve\admin\AdminControllerBase;
 use tsd\serve\SecurityGroup;
 use tsd\serve\MenuItem;
+use tsd\serve\Router;
 
 class CodeController extends AdminControllerBase
 {
@@ -18,7 +19,39 @@ class CodeController extends AdminControllerBase
     #[MenuItem('controller')]
     function showController()
     {
-        return $this->view();
+        $files = [];
+        $paths = glob(Router::CONTROLLER . DIRECTORY_SEPARATOR. '*.php');
+
+        foreach ($paths as $p)
+        {
+            $files[]=['path'=>$p, 'name'=>basename($p)];
+        }
+
+
+        return $this->view(['files' => $files]);
+    }
+
+    #[SecurityGroup("developer")]
+    function showControllerEdit(string $name)
+    {
+        $content = file_get_contents(Router::CONTROLLER . DIRECTORY_SEPARATOR.$name);
+
+        return $this->view(['content'=>$content, 'name'=>$name]);
+    }
+
+    #[SecurityGroup("developer")]
+    function doControllerEdit(string $name, string $content)
+    {
+        try
+        {
+            eval('?>' . $content);
+            file_put_contents(Router::CONTROLLER . DIRECTORY_SEPARATOR.$name, $content);
+            return $this->success('saved');
+        }
+        catch (\Error $e)
+        {
+            return $this->view(['content'=>$content, 'name'=>$name]);
+        }  
     }
 
     #[SecurityGroup("developer")]
